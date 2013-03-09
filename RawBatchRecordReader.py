32b42c8c27fe06e691bb34671669e4d2f4b7aa62
Numeraire79 <numeraire79@gmail.com>
2013-03-08 22:17:19 -0600

    TDD

--- RawBatchRecordReader.py
+++ RawBatchRecordReader.py
@@ -4,6 +4,11 @@ __author__ = 'blocke'
 class RawBatchRecordReader(object):
 
     def __init__(self, file_location):
-        f = open(file_location, "r")
 
-        f.close()
+        with open(file_location, 'r') as f:
+            for line in f:
+                self.trim_time_stamp_and_end(line)
+            f.close()
+
+    def trim_time_stamp_and_end(self, line):
+        print line[16: -1]
--- TestRawBatchRecordReader.py
+++ TestRawBatchRecordReader.py
@@ -1,16 +1,23 @@
+from BatchProcessingExceptions import BatchRecordInvalidDataTypeException
+
 __author__ = 'blocke'
 from unittest import TestCase
 from RawBatchRecordReader import RawBatchRecordReader
 
 
-
 class TestRawBatchRecordReader(TestCase):
 
-    def __init__(self, test_name):
-        TestCase.__init__(test_name)
-        self.raw = RawBatchRecordReader()
+#    def __init__(self, test_name):
+ #       TestCase.__init__(test_name)
+  #      self.raw = RawBatchRecordReader("C:\\Data\\BatchData\\smd_batch_MDDS_fut_4.out")
 
+    def test_file_does_not_exist(self):
+        loc = "C:\\Data\\BatchData\\smd_batch_MDDS_fut_4.out"
+        self.assertRaises(IOError, RawBatchRecordReader, loc)
 
+    def test_file_does_not_exist(self):
+        loc = "C:\\Data\\BatchData\\smd_batch_MDDS_fut.out"
+        self.assertRaises(BatchRecordInvalidDataTypeException, RawBatchRecordReader, loc)
 
 if __name__ == '__main__':
     unittest.main()
